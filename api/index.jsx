6657fddd04029c74fedd99cd1eae2bf727ac3f4d
Decon2022 <danielcontreras344@gmail.com>
2024-07-23 15:34:34 -0700

    Initial Commit

--- api/index.jsx
+++ api/index.jsx
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const { default: mongoose } = require('mongoose');
+require('dotenv').config()
+app.use(express.json)
 
 // might need ot change the origin
 app.use(cors({
@@ -10,13 +12,28 @@ app.use(cors({
 
 }));
 
+//this is for the data base 
+//mogoldb 57:46
+
+mongoose.connect('process.env.MONGO_URL')
+
+
+
+
 
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
 
+
+app.post('/register', (req,res) => {
+
+    const {name, email, password} = req.body;
+    res.json({name,email,password});
+})
+
 app.listen(4000);
 
 //45:25 having issue getting connect to nodemon
 
-// had to use node
+// had to use nodes
